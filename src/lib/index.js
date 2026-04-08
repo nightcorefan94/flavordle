ca5063514a4e923b52fe7184ff5a3d96fc2b62ba
test <>
2026-04-08 13:58:00 -0700

    test

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,20 @@
-// place files you want to import through the `$lib` alias in this folder.
+// place files you want to import through the `$lib` alias in this folder.exp
+export async function grabRandomProject() {
+    let fetcher = await fetch("https://ftpdb.jam06452.uk/api/random_projects?filter=stat_hot_score");
+    let random = await fetcher.json();
+    // let rand = random[Math.floor(Math.random() * random.length)];
+    return random;
+}
+
+export async function generate(data, prev) {
+    // let rand = data[Math.floor(Math.random() * data.length)];
+    let filter = data.filter((item) => item.total_hours != prev.total_hours);
+    let res;
+    if (filter.length == 0) res = {"total_hours":-1,"title":"debug","id":2};
+    res = filter[Math.floor(Math.random() * filter.length)];
+
+    return {
+        one: prev,
+        two: res,
+    }
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,68 @@
 <script>
+    import { onMount } from "svelte";
+    import { generate, grabRandomProject } from "$lib/index.js"
+
+    let project = $state({});
+    let other = $state({});
+    let text = $state(0);
+    /*
     let project = $state({"id":"1","title":"test","total_hours":0});
-    async function grabRandomProject() {
-        let random = await fetch("https://ftpdb.jam06452.uk/api/random_projects?filter=stat_hot_score")
-        let json = await random.json();
-        let rand = json[Math.floor(Math.random() * json.length)];
-        project = rand;
+    let other = $state({"id":"1","title":"test","total_hours":0});
+    */
+
+    async function shit() {        
+        let data = await grabRandomProject();
+        // let shit = data[Math.floor(Math.random() * data.length)];
+        let meow = await generate(data, other);
+        project = other;
+        other = meow.two;
     }
-    grabRandomProject();
+
+    onMount(async () => {
+        let data = await grabRandomProject();
+        let shit = data[Math.floor(Math.random() * data.length)];
+        let meow = await generate(data, shit);
+        console.log(meow);
+        project = meow.one;
+        other = meow.two;
+        // project = await grabRandomProject();
+    });
+
+    async function compare(think) {
+        if (think == "higher" && other.total_hours > project.total_hours) {
+            text += 1;
+            await shit();
+        } else if (think == "lower" && other.total_hours < project.total_hours) {
+            text += 1;
+            await shit();
+        } else {
+            text = 0;
+        }
+        // if (think == "higher") {
+        //     if (project.total_hours > other.total_hours) {
+                
+        //     } 
+        // } else {
+            
+        // }
+    }
+
 </script>
 
+<div>
+    <div>
+        <h1>{project.title} </h1>
+        <p>{project.total_hours} hours</p>
+    </div>
+    <div>
+        <h1>{other.title} has</h1>
+    </div>
+    <button onclick={async () => await compare("higher")} class="px-4 py-2 bg-lime-400 text-white rounded hover:bg-lime-500 transition">Higher</button>
+    <button onclick={async () => await compare("lower")} class="px-4 py-2 bg-red-400 text-white rounded hover:bg-red-500 transition">Lower</button>
+    <p>Score: {text}</p>
+</div>
 
-<h1>{project.title}</h1>
+<!-- <h1>{project.title}</h1>
 <p>{project.total_hours}</p>
-
+<h1>{other.title}</h1>
+<p>{other.total_hours}</p> -->
