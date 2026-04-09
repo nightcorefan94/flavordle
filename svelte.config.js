84098904ee17d7f8efa6dbc1b4ddb12fef71a6db
nightcorefan94 <274578546+nightcorefan94@users.noreply.github.com>
2026-04-08 20:25:39 -0700

    remove github shit actions

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,20 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	compilerOptions: {
-		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
-		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
-	},
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		paths: {
-			base: process.argv.includes('dev') ? '' : '/flavordle'
-		},
+		// default options are shown
 		adapter: adapter({
-			fallback: '404.html'
-		}),
-	},
+			// if true, will create a Netlify Edge Function rather
+			// than using standard Node-based functions
+			edge: false,
+
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app.
+			// if `edge` is true, this option cannot be used
+			split: false
+		})
+	}
 };
 
 export default config;
