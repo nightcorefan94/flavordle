652c57ab9ae8f96aed4ae5649e02764dc3a27661
test <>
2026-04-08 20:15:02 -0700

    fml

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.argv.includes('dev') ? '' : '/flavordle'
 		},
 		adapter: adapter({
 			fallback: '404.html'
